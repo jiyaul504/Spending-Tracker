0c52a63967c251bb52bfa33271c99767ad60a196
Jiyaul Haque Shekh Gaffar <jiyaulhaque.shekhgaffar@Impigertech.com>
2022-11-14 11:32:54 +0530

    Add project files.

---
+++ Spending Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spending_Tracker.Models
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Transaction> transactions{ get; set; }
+        public DbSet<Category> categories { get; set; }
+    }
+}
